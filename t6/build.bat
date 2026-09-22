30dbf86314c5d7adc187e907a129768505c26514
Hadi77KSA <135042368+Hadi77KSA@users.noreply.github.com>
2026-09-23 01:27:45 +0300

    T6ZM: Use custom ipak for MP-exclusive image
    menu_mp_lobby_customgame does not appear if the user does not have MP installed

--- t6/build.bat
+++ t6/build.bat
@@ -1,5 +1,8 @@
+SET unlinkerpath=%USERPROFILE%\source\repos\OpenAssetTools\build\bin\Release_x86\Unlinker.exe
 SET linkerpath=%USERPROFILE%\source\repos\OpenAssetTools\build\bin\Release_x86\Linker.exe
 SET gamepath=D:\SteamLibrary\steamapps\common\Call of Duty Black Ops II
+
+"%unlinkerpath%" --output-folder ".\zone_raw\plutonium_zm" --image-format "IWI" --include-assets "image" "%gamepath%\zone\all\ui_mp.ff"
 
 for %%f in ("zone_source\*.*") do (
     "%linkerpath%" --output-folder ".\zone_out" --load "%gamepath%\zone\all\ui_mp.ff" "%%~nf"
